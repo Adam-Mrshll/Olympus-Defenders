b1d2146c54a6941eeac3a8ecd4fdd6a7fe66f18d
Adam-Mrshll <adam.marshall@kingsway.school.nz>
2025-08-14 16:52:47 +1200

    Tutorial 13 Finished
    (No death effect because impact effect already exists)

--- olympus defenders/Assets/Scripts/Bullet.cs
+++ olympus defenders/Assets/Scripts/Bullet.cs
@@ -5,6 +5,9 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+
+    public int damage = 50; // amount of damage bullet does to enemy
+
     public float explosionRadius = 0f;
     public GameObject impactEffect;
 
@@ -47,8 +50,8 @@ public class Bullet : MonoBehaviour
         {
             Damage(target);
         }
-
-         Destroy(target.gameObject);
+        
+        Destroy(gameObject);
     }
 
     void Explode ()
@@ -65,7 +68,12 @@ public class Bullet : MonoBehaviour
 
     void Damage (Transform enemy)
     {
-        Destroy(enemy.gameObject);
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
     }
 
     void OnDrawGizmosSelected()
--- olympus defenders/Assets/Scripts/enemy.cs
+++ olympus defenders/Assets/Scripts/enemy.cs
@@ -4,12 +4,33 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
 
+    public int health = 100;
+
+    public int value = 50; // amount of money player gets from killing this enemy
+
     private Transform target;
     private int wavepointIndex = 0;
 
     void Start()
     {
         target = Waypoint.points[0];
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log($"Enemy takes {damage} damage. Health before: {health}");
+        health -= damage;
+        Debug.Log($"Enemy health now: {health}");
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        PlayerStats.Money += value;
+        Destroy(gameObject);
     }
 
     void Update()
