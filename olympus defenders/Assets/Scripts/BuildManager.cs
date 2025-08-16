900faa2afaf134616348fa15d6573af6c1123969
Adam-Mrshll <adam.marshall@kingsway.school.nz>
2025-08-16 16:23:08 +1200

    Tutorial 18 (selection) Finished
    No errors (left out vanity such as animations due to time constraints

--- olympus defenders/Assets/Scripts/BuildManager.cs
+++ olympus defenders/Assets/Scripts/BuildManager.cs
@@ -19,6 +19,9 @@ public class BuildManager : MonoBehaviour
     public GameObject hephaestusTurretPrefab; // Hephaestus turret
 
     private TurretBlueprint turretToBuild;
+    private Node selectedNode;
+
+    public NodeUI nodeUI;
 
     public bool CanBuild { get { return turretToBuild != null; } } // property, (can only get something from this variable)
     public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } } 
@@ -39,9 +42,29 @@ public class BuildManager : MonoBehaviour
         Debug.Log("Tower Placed! Money left: $" + PlayerStats.Money);
     }
 
+    public void SelectNode (Node node)
+    {
+        if (selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }    
+        selectedNode = node;
+        turretToBuild = null;
+
+        nodeUI.SetTarget(node);
+    }
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();
+    }
+
     public void SelectTurretToBuild (TurretBlueprint turret)
     {
         turretToBuild = turret;
+        DeselectNode();
     }
 
 }
